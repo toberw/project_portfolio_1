4ab8ae18bb6d028facfc8360fe2f957a59198a70
wtober <wvtober@student.fullsail.edu>
2019-09-20 02:26:21 -0400

    started reader to detect user keys and gain xp per click.. Need to review program plan befoe I go further.

---
+++ RPGTyper/RPGTyper/Player.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace RPGTyper
+{
+    class Player
+    {
+        private string _name;
+        private int _gold;
+        private int _xp;
+
+        public int XP
+        {
+            get { return _xp; }
+            set { _xp = value; }
+        }
+
+        public int Gold
+        {
+            get { return _gold; }
+            set { _gold = value; }
+        }
+
+        public Player(string name, int gold = 0)
+        {
+            _name = name;
+            _gold = gold;
+            _xp = 0;
+        }
+
+        
+    }
+}
--- RPGTyper/RPGTyper/Program.cs
+++ RPGTyper/RPGTyper/Program.cs
@@ -6,7 +6,24 @@ namespace RPGTyper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player currentPlayer = new Player("John Doe");
+            bool programRunning = true;
+
+            while (programRunning)
+            {
+                Console.WriteLine("This is the start of things. ");
+
+                while (Console.ReadKey().Key != ConsoleKey.X)
+                {
+                    Console.Clear();
+                    currentPlayer.XP += 1;
+                    Console.WriteLine($"Experience: {currentPlayer.XP}");
+                }
+
+                programRunning = false;
+
+
+            }
         }
     }
 }
