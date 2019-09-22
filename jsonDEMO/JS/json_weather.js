6462bfcb96e00242d27ed62a2a8beb99dd61dfe7
wtober <wvtober@student.fullsail.edu>
2019-09-22 01:54:59 -0400

    completed json practice

---
+++ jsonDEMO/JS/json_weather.js
@@ -0,0 +1,35 @@
+var data = {
+    "results": {
+        "units": {
+            "distance" : "mi",
+            "pressure" : "ins",
+            "speed" : "mph",
+            "temperature" : "F"
+        },
+        "wind": {
+            "chill" : "76",
+            "direction" : "360",
+            "speed" : "5"
+        },
+        "atmosphere": {
+            "humidity" : "48",
+            "pressure" : "29.65",
+            "rising" : "0",
+            "visibility" : "10"
+        },
+        "astronomy": {
+            "sunrise" : "7:26 AM",
+            "sunset" : "6:51 PM"
+        },
+        "condition": {
+            "code" : "33",
+            "date" : "Fri, 17 Oct 2014 5:53 am EST",
+            "temp" : "Fair"
+        },
+        "forecast" : [
+            {"code" : "30", "date" : "17 Oct 2014", "day" : "Fri", "high" : "80", "low" : "62", "text" : "Sunny"},
+            {"code" : "30", "date" : "18 Oct 2014", "day" : "Sat", "high" : "82", "low" : "63", "text" : "Partly Cloudy"},
+            {"code" : "30", "date" : "9 Oct 2014", "day" : "Sun", "high" : "85", "low" : "65", "text" : "Partly Cloudy"}
+        ]
+    }
+}
